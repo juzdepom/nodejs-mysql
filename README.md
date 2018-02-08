4f6b0b8db00ad9b06ebd6dc9c5181f128f84f475
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-02-08 15:31:56 -0500

    see readme

---
+++ README.md
@@ -0,0 +1,6 @@
+Based off of this Traversy Youtube [Tutorial]()
+
+Unfinished because when trying to connect to mySQL would give this error:
+```diff
++ ECONNREFUSED 127.0.0.1:3306
+```
--- index.js
+++ index.js
@@ -1,42 +1,46 @@
 const express = require('express');
 const mysql = require('mysql');
 
+// Create connection
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'nodemysql'
+});
+
+// Connect
+
+// db.connect()
+
+db.connect((err) => {
+    if(err){
+        console.log('THERE WAS AN ERROR: ' + err)
+        throw err;
+    }
+    console.log('MySql connected...')
+
+});
+
+// Create DB
+
 const app = express();
+
+// app.get('/createdb', (req, res) => {
+//     db.query(sql, (err, result) => {
+//         if(err){
+//             throw err
+//         } else {
+//             res.send('Database created...');
+//             console.log(result);
+//         }
+//     })
+// })
 
 app.listen('3000', () => {
     console.log("Server started on port 3000")
 })
 
-// Create connection
 
-// const db = mysql.createConnection({
-//     host: '127.0.0.1',
-//     user: 'root',
-//     password: 'password',
-// });
-
-// Connect
-
-// db.connect((err) => {
-//     if(err){
-//         console.log('THERE WAS AN ERROR: ' + err)
-//         throw err;
-//     }
-//     console.log('MySql connected...')
-
-// });
-
-// Create DB
-
-// app.get('/createdb', (req, res) => {
-//     let sql = 'CREATE DATABASE nodejsmysql';
-//     db.query(sql, (err, result) => {
-//         if(err){
-//             throw err
-//         } else {
-//             res.send('databased created...');
-//             console.log(result);
-//         }
-//     })
-// })
 
